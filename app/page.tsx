fb7d55885ccf1589b01cd0704a2f9cf0d76ec3c4
TsipiG <Tsipig@sncentral.org>
2025-04-07 21:48:59 +0300

    Addding David

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,8 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Hello NEXT
+            Hello David!!!!!!!!!!!!
+            
           </li>
         </ol>
 
